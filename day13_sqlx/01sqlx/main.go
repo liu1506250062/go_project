75fef3da127023b4e522e3cde2290f3559d00df4
lyz <1506250062@qq.com>
2020-12-15 22:16:57 +0800

    sqlx

--- day13_sqlx/01sqlx/main.go
+++ day13_sqlx/01sqlx/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
@@ -8,10 +9,11 @@ import (
 
 var db *sqlx.DB
 
-type user struct {
-	id   int
-	age  int
-	name string
+//PS: 这里需要首字母大写   为了让外部文件访问
+type Users struct {
+	Id   int
+	Name string
+	Age  int
 }
 
 //连接数据库
@@ -30,15 +32,77 @@ func initDB() (err error) {
 }
 
 //单行数据查询
+// 查询单条数据示例
 func queryRowDemo() {
-	sqlStr := `select * from users where id = ?`
-	var u user
+	sqlStr := "select * from users where id=?"
+	var u Users
 	err := db.Get(&u, sqlStr, 1)
+	if err != nil {
+		fmt.Printf("get failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("id:%d name:%s age:%d\n", u.Id, u.Name, u.Age)
+}
+
+
+//获取多行数据
+func queryMultiRowDemo() {
+	sqlStr := `select * from users where id >?`
+	var users []Users
+	err := db.Select(&users, sqlStr, 0)
 	if err != nil {
 		fmt.Printf("queryRowDemo failed,err :%v\n", err)
 		return
 	}
-	fmt.Printf("id:%d name:%s age:%d\n", u.id, u.name, u.age)
+	fmt.Printf("%#v\n", users)
+}
+
+
+func insertRowDemo() {
+	sqlStr := "insert into users(name, age) values (?,?)"
+	ret, err := db.Exec(sqlStr, "沙河小王子", 19)
+	if err != nil {
+		fmt.Printf("insert failed, err:%v\n", err)
+		return
+	}
+	theID, err := ret.LastInsertId() // 新插入数据的id
+	if err != nil {
+		fmt.Printf("get lastinsert ID failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("insert success, the id is %d.\n", theID)
+}
+
+// 更新数据
+func updateRowDemo() {
+	sqlStr := "update users set age=? where id = ?"
+	ret, err := db.Exec(sqlStr, 39, 6)
+	if err != nil {
+		fmt.Printf("update failed, err:%v\n", err)
+		return
+	}
+	n, err := ret.RowsAffected() // 操作影响的行数
+	if err != nil {
+		fmt.Printf("get RowsAffected failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("update success, affected rows:%d\n", n)
+}
+
+// 删除数据
+func deleteRowDemo() {
+	sqlStr := "delete from users where id = ?"
+	ret, err := db.Exec(sqlStr, 6)
+	if err != nil {
+		fmt.Printf("delete failed, err:%v\n", err)
+		return
+	}
+	n, err := ret.RowsAffected() // 操作影响的行数
+	if err != nil {
+		fmt.Printf("get RowsAffected failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("delete success, affected rows:%d\n", n)
 }
 
 func main() {
@@ -46,6 +110,59 @@ func main() {
 	if err != nil {
 		fmt.Printf("init DB failed, err %v\n", err)
 	}
-	queryRowDemo()
+	//queryRowDemo()
+	//queryMultiRowDemo()
+	//insertRowDemo()
+	//updateRowDemo()
+	//deleteRowDemo()
 
+}
+
+
+//事务操作
+func transactionDemo2()(err error) {
+	tx, err := db.Beginx() // 开启事务
+	if err != nil {
+		fmt.Printf("begin trans failed, err:%v\n", err)
+		return err
+	}
+	defer func() {
+		if p := recover(); p != nil {
+			tx.Rollback()
+			panic(p) // re-throw panic after Rollback
+		} else if err != nil {
+			fmt.Println("rollback")
+			tx.Rollback() // err is non-nil; don't change it
+		} else {
+			err = tx.Commit() // err is nil; if Commit returns error update err
+			fmt.Println("commit")
+		}
+	}()
+
+	sqlStr1 := "Update user set age=20 where id=?"
+
+	rs, err := tx.Exec(sqlStr1, 1)
+	if err!= nil{
+		return err
+	}
+	n, err := rs.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if n != 1 {
+		return errors.New("exec sqlStr1 failed")
+	}
+	sqlStr2 := "Update user set age=50 where i=?"
+	rs, err = tx.Exec(sqlStr2, 5)
+	if err!=nil{
+		return err
+	}
+	n, err = rs.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if n != 1 {
+		return errors.New("exec sqlStr1 failed")
+	}
+	return err
 }
