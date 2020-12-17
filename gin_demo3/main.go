8f59a9a75efed9071bc6200c63e9ea2db7ea3d72
lyz <1506250062@qq.com>
2020-12-17 23:10:55 +0800

    gin

--- gin_demo3/main.go
+++ gin_demo3/main.go
@@ -1,1 +1,22 @@
-package gin_demo3
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+
+//05  4minutes 50 5s
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("qwe")
+}
+
+func main() {
+	http.HandleFunc("/", sayHello)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		fmt.Println("HTTP Server start failed ,err :%")
+		return
+	}
+}
