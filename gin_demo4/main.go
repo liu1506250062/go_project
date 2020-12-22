0b578e241ba917aff773b35c4d33877679ab0aaa
lyz <1506250062@qq.com>
2020-12-22 22:43:06 +0800

    模板继承

--- gin_demo4/main.go
+++ gin_demo4/main.go
@@ -11,7 +11,7 @@ func f1(w http.ResponseWriter, r *http.Request) {
 	//定义一个函数 kua
 	//要么只有一个返回值  要么有两个返回值 第二个返回值 必须是 error 类型
 	k := func(name string) (string, error) {
-		return name + "123",nil
+		return name + "123", nil
 	}
 
 	//定义模板
@@ -19,7 +19,7 @@ func f1(w http.ResponseWriter, r *http.Request) {
 
 	//告诉模板 有一个自定义函数
 	t.Funcs(template.FuncMap{
-		"kua":k,
+		"kua": k,
 	})
 
 	//解析模板
@@ -34,8 +34,26 @@ func f1(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, name)
 }
 
+func demo1(w http.ResponseWriter, r *http.Request) {
+
+	//定义模板
+
+	//解析模板
+	t, err := template.ParseFiles("t.tmpl", "ul.tmpl") //同时解析多个文件 要把被包含的写在后边
+
+	if err != nil {
+		fmt.Printf("faild: %v", err)
+		return
+	}
+	name := "小王子"
+
+	//渲染模板
+	t.Execute(w, name)
+}
+
 func main() {
 	http.HandleFunc("/", f1)
+	http.HandleFunc("/tmplDemo", demo1)
 	err := http.ListenAndServe(":9080", nil)
 	if err != nil {
 		fmt.Printf("Http Server not start  %v", err)
@@ -43,5 +61,3 @@ func main() {
 	}
 }
 
-
-//06  15.28 嵌套模板
