797d96328aeefd6f2e2c4cf4e4428a310b41e666
lyz <1506250062@qq.com>
2021-01-31 17:07:18 +0800

    test

--- gin_demo_note/models/todo.go
+++ gin_demo_note/models/todo.go
@@ -24,8 +24,10 @@ func GetAllTodo() (todoList []*Todo, err error) {
 	return
 }
 
-func GetATodo(id string) (todo *Todo, err error) {
-	if err = dao.DB.Where("id = ?", id).First(&todo).Error; err != nil {
+
+func GetATodo(id string)(todo *Todo, err error){
+	todo = new(Todo)
+	if err = dao.DB.Debug().Where("id=?", id).First(todo).Error; err!=nil{
 		return nil, err
 	}
 	return
