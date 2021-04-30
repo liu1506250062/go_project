2c47a17247378cb750195ec7d3565b933a4552d9
liu1506250062 <1506250062@qq.com>
2021-04-30 17:49:17 +0800

    test

---
+++ test.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+}
+
+func f1() {
+	fmt.Println("hello world")
+}
+
+func f2(name string) {
+	fmt.Println("hello %s\n", name)
+}
+
+func f3(x int, y int) int {
+	sum := x + y
+	return sum
+}
