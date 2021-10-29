554c2978115636cfa9d7edc6b9f657932dc86416
liuyaozong@jiayuan.com <liuyaozong@jiayuan.com>
2021-10-29 18:55:37 +0800

    test

--- new_project/chat/client.go
+++ new_project/chat/client.go
@@ -1,6 +1,10 @@
 package main
 
-import "net"
+import (
+	"flag"
+	"fmt"
+	"net"
+)
 
 type Client struct {
 	ServerIp   string
@@ -12,11 +16,45 @@ type Client struct {
 func NewClient(serverIp string, serverPort int) *Client {
 
 	//创建客户对象
+	client := &Client{
+		ServerIp:   serverIp,
+		ServerPort: serverPort,
+	}
 
 	//创建Server
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", serverIp, serverPort))
+	if err != nil {
+		fmt.Println("net.Dial error", err)
+	}
+	client.conn = conn
+
+	//返回对象
+	return client
 
 }
 
+var serverIp string
+var serverPort int
+
+func init() {
+	flag.StringVar(&serverIp, "ip", "127.0.0.1", "设置服务器IP地址（默认是127.0.0.1）")
+	flag.IntVar(&serverPort, "port", 8888, "设置服务器端口（默认是8888）")
+}
+
 func main() {
+
+	//命令行解析
+	flag.Parse()
+
+
+	client := NewClient("127.0.0.1", 8888)
+	if client == nil {
+		fmt.Println(">>>>>>>>>>server connect  error....")
+	}
+
+	fmt.Println(">>>>> server connect success")
+
+	//启动客户端的服务
+	select {}
 
 }
